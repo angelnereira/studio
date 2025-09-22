126bb405f38eaf63b3f136683d680eb340a078e9
Ángel Nereira <angelnereira15@gmail.com>
2025-09-22 12:13:13 +0000

    que el carrusel solo se detenca solo cuando el cursor pase sobre este ti

--- src/components/home-sections.tsx
+++ src/components/home-sections.tsx
@@ -36,7 +36,7 @@ export function SkillsSection() {
             </AnimatedDiv>
              <div className="group/container relative mt-12 w-full overflow-hidden px-4">
                 <div className="absolute inset-y-0 left-0 z-10 w-[7.5%] bg-gradient-to-r from-background to-transparent" />
-                <div className="flex h-40 w-max animate-marquee items-center p-4 transition-all duration-500 ease-geist hover:[animation-play-state:paused]">
+                <div className="flex h-40 w-max animate-marquee items-center p-4 transition-all duration-500 ease-geist group-hover/container:[animation-play-state:paused]">
                   {[...skills, ...skills].map((skill, index) => (
                     <Tooltip key={`${skill.slug}-tooltip-${index}`}>
                       <TooltipTrigger asChild>
@@ -44,7 +44,7 @@ export function SkillsSection() {
                           <div
                             className="group/item relative mx-2 flex w-36 flex-col items-center justify-center text-center transition-all duration-500 ease-geist h-28 group-hover/container:opacity-75 hover:!opacity-100"
                           >
-                            <div className="flex h-24 w-24 flex-shrink-0 cursor-pointer items-center justify-center rounded-lg bg-secondary p-6 transition-all duration-300 hover:shadow-primary/20 hover:shadow-2xl">
+                            <div className="flex h-24 w-24 flex-shrink-0 cursor-pointer items-center justify-center rounded-lg bg-secondary p-6 transition-all duration-300">
                               <skill.icon className='h-10 w-10 text-primary' />
                             </div>
                           </div>
