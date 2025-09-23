3fa429dedbb0bdd63ea322fa7593d5504438e54e
Ángel Nereira <angelnereira15@gmail.com>
2025-09-23 22:45:13 +0000

    add middleware.ts

---
+++ src/middleware.ts
@@ -0,0 +1,14 @@
+import { NextResponse, NextRequest } from 'next/server';
+import { get } from '@vercel/edge-config';
+
+export const config = { 
+  matcher: '/welcome' 
+};
+
+export async function middleware(request: NextRequest): Promise<NextResponse> {
+  const greeting: string | undefined = await get('greeting');
+  
+  // NextResponse.json requires at least Next v13.1 or
+  // enabling experimental.allowMiddlewareResponseBody in next.config.js
+  return NextResponse.json(greeting);
+}
